b4ee60b7755e7124216200675744d007c7af3782
lukasimonishvili <37000830+lukasimonishvili@users.noreply.github.com>
2025-05-12 17:52:16 +0200

    fin

--- FizzBuzz/src/Main.java
+++ FizzBuzz/src/Main.java
@@ -1,7 +1,38 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
+    public static void helloWorld() {
+        System.out.println("Hello World");
+    }
+
+    public static void sumOfTwoNumber(int num1, int num2) {
+        System.out.println(num1 + num2);
+    }
+
+    public static void primeNumber() {
+        Scanner userNumber = new Scanner(System.in);
+        System.out.println("Please enter you number");
+        Boolean result = true;
+        int number = userNumber.nextInt();
+        if(number <= 1) {
+            result = false;
+        }else if (number == 2){
+            result = true;
+        }else if(number % 2 == 0) {
+            result = false;
+        }else {
+            for(int i = 3; i <= Math.sqrt(number); i += 2) {
+                if(number % i == 0) {
+                    result = false;
+                }
+            }
+        }
+        String isOrNot = result ? "" : "not";
+        System.out.println(number + " is " + isOrNot + " a prime number");
+    }
     public static void fizzBuzz(int number) {
         String result = "";
         for(int i = 1; i <= number; i++) {
@@ -23,6 +54,9 @@ public class Main {
         System.out.println(result);
     }
     public static void main(String[] args) {
+        helloWorld();
+        sumOfTwoNumber(20, 10);
         fizzBuzz(17);
+        primeNumber();
     }
 }
