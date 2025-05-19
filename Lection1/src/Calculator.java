adae6cbf56cbfbefb9511980a690923503a2af8e
lukasimonishvili <37000830+lukasimonishvili@users.noreply.github.com>
2025-05-19 18:34:12 +0200

    added support for negative numbers

--- Lection1/src/Calculator.java
+++ Lection1/src/Calculator.java
@@ -5,49 +5,50 @@ public class Calculator {
         String left = "";
         String right = "";
 
-
-        for(int i = index - 1; i >= 0; i--) {
-            if(Character.toString(input.charAt(i)).matches("^[0-9.]+$")){
-                left = input.charAt(i) + left;
-            }else {
+        for (int i = index + 1; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (Character.toString(ch).matches("[0-9.]")) {
+                right += ch;
+            } else if (ch == '-' && right.isEmpty()) {
+                right += ch;
+            } else {
                 break;
             }
         }
 
-        for(int i = index + 1; i < input.length(); i++) {
-            if(Character.toString(input.charAt(i)).matches("^[0-9.]+$")){
-                right += input.charAt(i);
-            }else {
+        for (int i = index - 1; i >= 0; i--) {
+            char ch = input.charAt(i);
+            if (Character.toString(ch).matches("[0-9.]")) {
+                left = ch + left;
+            } else if (ch == '-' && (i == 0 || "+-*/(".contains("" + input.charAt(i - 1)))) {
+                left = ch + left;
+                break;
+            } else {
                 break;
             }
         }
+
         Double leftDouble = Double.valueOf(left);
         Double rightDouble = Double.valueOf(right);
         double result = operation.equals("*") ? leftDouble * rightDouble :
-                        operation.equals("/") ? leftDouble / rightDouble :
+                operation.equals("/") ? leftDouble / rightDouble :
                         operation.equals("+") ? leftDouble + rightDouble :
-                        leftDouble - rightDouble;
+                                leftDouble - rightDouble;
 
         return input.replace(left + operation + right, Double.toString(result));
     }
 
     private double calculate(String input) {
-        while(input.contains("(")) {
+        while (input.contains("(")) {
             int left = input.indexOf("(");
             int right = -1;
             int nestedBracketCount = 0;
-            for(int i = left + 1; i < input.length(); i++) {
-                if(input.charAt(i) == '(') {
+            for (int i = left + 1; i < input.length(); i++) {
+                if (input.charAt(i) == '(') {
                     nestedBracketCount++;
-                    continue;
-                }
-
-                if(input.charAt(i) == ')' && nestedBracketCount > 0) {
+                } else if (input.charAt(i) == ')' && nestedBracketCount > 0) {
                     nestedBracketCount--;
-                    continue;
-                }
-
-                if(input.charAt(i) == ')') {
+                } else if (input.charAt(i) == ')' && nestedBracketCount == 0) {
                     right = i + 1;
                     break;
                 }
@@ -57,31 +58,30 @@ public class Calculator {
             input = input.replace(subString, Double.toString(calculate(input.substring(left + 1, right - 1))));
         }
 
-        while(!input.matches("^[0-9.]+$")){
-            if(input.contains("*")) {
+        while (!input.matches("^-?[0-9.]+$")) {
+            if (input.contains("*")) {
                 input = numberParser(input.indexOf("*"), input, "*");
             } else if (input.contains("/")) {
                 input = numberParser(input.indexOf("/"), input, "/");
             } else if (input.contains("+")) {
                 input = numberParser(input.indexOf("+"), input, "+");
             } else if (input.contains("-")) {
-                input = numberParser(input.indexOf("-"), input, "-");
+                int minusIndex = input.indexOf("-", 1);
+                if (minusIndex == -1) break;
+                input = numberParser(minusIndex, input, "-");
             }
         }
 
         return Double.parseDouble(input);
     }
 
-
-
     public void calculator() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Please enter math problem");
-        //String input = scanner.nextLine();
-        String input = "3.5 + 2.8 * (4 - 1.2  * 2) / 2 + (10 - 2)";
+        System.out.println("Please enter math problem:");
+        String input = scanner.nextLine();
         input = input.replaceAll(" ", "");
-        System.out.println(input);
+        System.out.println("Parsed input: " + input);
         double result = calculate(input);
         System.out.println("Answer is " + result);
     }
